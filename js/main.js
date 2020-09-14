3319631cce6cb22696069472ce4b72ddf1fbc2a8
Tanya Grunicheva <eiforiaa.2010@mail.ru>
2020-09-14 12:05:11 +0300

    пропали картинки, все стало хуже

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
 $(function){
-	$('.owl-carousel').owlCarousel({
+$('.owl-carousel').owlCarousel({
     loop:true,
-    margin:30,
+    margin:10,
     nav:true,
     responsive:{
         0:{
             items:1
         },
         600:{
-            items:1
+            items:3
         },
         1000:{
-            items:2
+            items:5
         }
     }
 })
 }
+
+// 	$('.contributor-carousel').owlCarousel({
+// 	    loop:true,
+// 	    margin:0,
+// 	    nav:true,
+// 	    dots: false,
+// 	    navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+// 	    // startPosition: 1,
+// 	    responsive:{
+// 	        0:{
+// 	            items:1
+// 	        },
+// 	        600:{
+// 	            items:1
+// 	        },
+// 	        1000:{
+// 	            items:1
+// 	        }
+// 	    }
+// 	});
+
+
+// 	$('ul.tabs__caption').on('click', 'li:not(.active)', function(e) {
+// 		e.preventDefault();
+//     $(this)
+//       .addClass('active').siblings().removeClass('active')
+//       .closest('section.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+//   });
+
+// });
